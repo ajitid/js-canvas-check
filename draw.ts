e112af6c244eadbf2101dcc47119943b632bbaa8
Ajit <zlksnkwork@gmail.com>
2025-04-20 22:24:31 +0530

    separate

--- draw.ts
+++ draw.ts
@@ -1,1 +1,26 @@
-import type { SKRSContext2D } from "@napi-rs/canvas";
+let displayFPSPrev = performance.now()
+
+export function draw() {
+  const { c } = globalThis
+  // clear canvas:
+
+  // TODO add this when using skia-canvas
+  // ctx.reset();
+
+  c.clearRect(0, 0, w, h)
+  // or
+  // ctx.fillStyle = "black";
+  // ctx.fillRect(0, 0, width, height);
+
+  // draw commands:
+
+  c.fillStyle = 'blue'
+  c.fillRect(0, 0, w, h)
+
+  c.fillStyle = 'white'
+
+  c.font = 'Inter 44px'
+  const now = performance.now()
+  c.fillText((1000 / (now - displayFPSPrev)).toFixed(2), 40, 40)
+  displayFPSPrev = now
+}
--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 import { setImmediate, setTimeout } from 'node:timers/promises'
 import sdl from '@kmamal/sdl'
 import { createCanvas } from '@napi-rs/canvas'
+import { draw } from './draw'
 
 function updateCanvas() {
   if (globalThis.canvas == null) {
@@ -97,31 +98,5 @@ async function main() {
 function handleInput() {}
 
 function update(fixedTimeStep: number) {}
-
-let displayFPSPrev = performance.now()
-export function draw() {
-  const { c } = globalThis
-  // clear canvas:
-
-  // TODO add this when using skia-canvas
-  // ctx.reset();
-
-  c.clearRect(0, 0, w, h)
-  // or
-  // ctx.fillStyle = "black";
-  // ctx.fillRect(0, 0, width, height);
-
-  // draw commands:
-
-  c.fillStyle = 'blue'
-  c.fillRect(0, 0, w, h)
-
-  c.fillStyle = 'white'
-
-  c.font = 'Inter 44px'
-  const now = performance.now()
-  c.fillText((1000 / (now - displayFPSPrev)).toFixed(2), 40, 40)
-  displayFPSPrev = now
-}
 
 main()
