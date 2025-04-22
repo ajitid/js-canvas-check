539aa6235965c9da887f7a86d46685228fbd0b85
Ajit <zlksnkwork@gmail.com>
2025-04-22 09:12:05 +0530

    add log for low 1% FPS

--- draw.ts
+++ draw.ts
@@ -1,12 +1,22 @@
 let prevTimestamp = performance.now()
 
 let minFps = Infinity,
-  maxFps = 0
+  maxFps = 0,
+  fpsList: number[] = []
 setInterval(() => {
-  console.log(minFps, maxFps)
+  // https://www.reddit.com/r/buildapc/comments/1dbdquf/what_do_1_low_and_01_low_mean_in_fps/
+  fpsList = fpsList.sort()
+  const lowFpsCount = Math.round(fpsList.length * 0.01) // checking 1% low
+  let lowFps = 0
+  for (let i = 0; i < lowFpsCount; i++) lowFps += fpsList[i]!
+  lowFps = lowFps / lowFpsCount
+  lowFps = parseFloat(lowFps.toFixed(2))
+
+  console.log({ lowFps, minFps, maxFps })
   minFps = Infinity
   maxFps = 0
-}, 2000)
+  fpsList = []
+}, 4000)
 
 export function draw() {
   const c = canvasContext,
@@ -40,4 +50,5 @@ function showFps() {
   const fps = parseFloat(fpsStr)
   minFps = Math.min(minFps, fps)
   maxFps = Math.max(maxFps, fps)
+  fpsList.push(fps)
 }
